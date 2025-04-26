4cf4b78dd4d580763bbf6e47571615127158f305
MOSAED-D1 <mosaeddoukhi@gmail.com>
2025-04-26 08:01:54 +0300

    Update assignment_1.py

--- football_dictionaries/assignment_1.py
+++ football_dictionaries/assignment_1.py
@@ -1,2 +1,11 @@
 def players_as_dictionaries(squads_list):
     pass
+def players_as_dictionaries(squads_list):
+    keys = ['number', 'position', 'name', 'date_of_birth', 'caps', 'club', 'country', 'club_country', 'year']
+    players_list = []
+
+    for player in squads_list:
+        player_dict = dict(zip(keys, player))
+        players_list.append(player_dict)
+
+    return players_list
