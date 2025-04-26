a0fd5a76da125eeae01cc7ff083cfaf36466f243
MOSAED-D1 <mosaeddoukhi@gmail.com>
2025-04-26 08:03:48 +0300

    Update assignment_2.py

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,13 @@
 def players_by_position(squads_list):
     pass
+def group_players_by_position(squads_data):
+    players = players_list_to_dicts(squads_data)
+    grouped_by_position = {}
+
+    for player in players:
+        position = player['position']
+        if position not in grouped_by_position:
+            grouped_by_position[position] = []
+        grouped_by_position[position].append(player)
+
+    return grouped_by_position
